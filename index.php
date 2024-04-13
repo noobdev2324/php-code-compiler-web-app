6f43619ce06cbc99797e090bc7ceffb6a81f2d69
Noob Dev <web3noobdeveloper@gmail.com>
2024-04-13 03:02:47 -0700

    index file added

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Compiler</title>
+</head>
+<body>
+    <h1>PHP Compiler</h1>
+</body>
+</html>
